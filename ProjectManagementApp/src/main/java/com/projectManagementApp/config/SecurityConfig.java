20ca641334e613b81310695c603f44dfc0c39dcc
Achamanp <144558579+Achamanp@users.noreply.github.com>
2025-07-01 03:17:10 +0530

    Update SecurityConfig.java

--- ProjectManagementApp/src/main/java/com/projectManagementApp/config/SecurityConfig.java
+++ ProjectManagementApp/src/main/java/com/projectManagementApp/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("http://localhost:5173");
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://spontaneous-chaja-2084a3.netlify.app");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setMaxAge(3600L);
